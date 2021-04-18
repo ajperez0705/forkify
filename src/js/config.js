5c38d5b71e5ae18cc5ab04c4b3b2b2d277605945
Andrew <ajperez0705@gmail.com>
2021-04-18 19:36:33 -0400

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '07e453b6-0439-4695-8aa5-e0644c84ec2d';
+export const MODAL_CLOSE_SEC = 2.5;
